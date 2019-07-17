db46ae23c61fdbb2c29cf27af53e90c24f7318ea
Duc Khang Nguyen <duckhang2210@gmail.com>
2019-07-17 14:09:43 -0500

    show correct,wrong, times up and picture for each result

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,7 @@ var A = $('#A');
 var B = $('#B');
 var C = $('#C');
 var D = $('#D');
+var showAnswer = $('#answer');
 var counter = $('#counter');
 var scoreBoard = $('#scoreBoard');
 var result = $('#result');
@@ -21,7 +22,7 @@ var questions = [
         C: "Mick Taylor",
         D: "Hank Pym",
         correct: "D",
-        pic: ""
+        pic: "Hank Pym.jpg"
     },
     {
         question: "What is the real name of the member of the Uncanny X-Men known as Nightcrawler?",
@@ -30,7 +31,7 @@ var questions = [
         C: "Charles Xavier",
         D: "Taylor Swift",
         correct: "A",
-        pic: ""
+        pic: "Kurt Wagner.jpg"
     },
     {
         question: "What is the Avenger known as Hawkeye's real name?",
@@ -39,7 +40,7 @@ var questions = [
         C: "Oliver Queen",
         D: "Robin Hood",
         correct: "B",
-        pic: ""
+        pic: "Clint Barton.jpg"
     },
     {
         question: "Who wiped out almost all of the mutants by saying 3 words 'No More Mutants'?",
@@ -48,7 +49,7 @@ var questions = [
         C: "Doctor Strange",
         D: "The Galactus",
         correct: "A",
-        pic: ""
+        pic: "Scarlett Witch.jpg"
     },
     {
         question: "The clawed superhero Wolverine is what nationality?",
@@ -57,7 +58,7 @@ var questions = [
         C: "Australian",
         D: "Mexican",
         correct: "A",
-        pic: ""
+        pic: "Canadian.jpg"
     },
     {
         question:"Star-Lord's mother is from Earth and his father is from:",
@@ -66,7 +67,7 @@ var questions = [
         C: "Scarif",
         D: "Asgard",
         correct: "A",
-        pic: ""
+        pic: "Spartax.jpg"
     },
     {
         question:"Which female character took on the role of 'Thor' when the original was unable?",
@@ -75,7 +76,7 @@ var questions = [
         C: "Jane Foster",
         D: "Mary Jane",
         correct: "C",
-        pic: ""
+        pic: "Jane Foster.jpg"
     },
     {
         question:"Peter Parker works as a photographer for:",
@@ -84,7 +85,7 @@ var questions = [
         C: "The New York Times",
         D: "The Avenger",
         correct: "B",
-        pic: ""
+        pic: "The Daily Bugle.jpg"
     },
     {
         question:"Who is Professor Xavier's step-brother?",
@@ -93,7 +94,7 @@ var questions = [
         C: "Juggernaut",
         D: "P.Xavier",
         correct: "C",
-        pic: ""
+        pic: "Juggernault.jpg"
     },
     {
         question:"Before turning to a life of crime, Mysterio was:",
@@ -102,7 +103,7 @@ var questions = [
         C: "A magician",
         D: "A special effects artist",
         correct: "D",
-        pic: ""
+        pic: "Mysterio.jpg"
     },
     {
         question:"Dr. Doom went to the same college as:",
@@ -111,7 +112,7 @@ var questions = [
         C: "Reed Richards",
         D: "Bruce Banner",
         correct: "C",
-        pic: ""
+        pic: "Reed Richard.jpg"
     },
     {
         question:"Who paid Mac Gargan to become the Scorpion?",
@@ -120,16 +121,16 @@ var questions = [
         C: "Dr. Otto",
         D: "J. Jonah Jameson",
         correct: "D",
-        pic: ""
+        pic: "Scorpion.jpg"
     },
     {
-        question:"Who got the title 'The Superior Spider Man'?",
+        question:"Who got the title 'The Superior Spider-Man'?",
         A: "Peter Parker",
         B: "Mysterio",
         C: "Miles Morales",
         D: "Otto Octavius",
         correct: "D",
-        pic: ""
+        pic: "Superior spider man.jpg"
     }
     
 ];
@@ -139,22 +140,22 @@ var lastQuestion = questions.length - 1;
 var checkingQuestion ;
 var count;
 var countDown;
-let score;
+var score;
+var timeout;
 //Start Game
 function startGame(){
+    clearInterval(timeout);
     checkingQuestion = 0;
     score = 0;
     count = 20;
+    showAnswer.css('display','none');
     result.css('display', 'none');
     startTheGame.css('display', 'none');
     quiz.css('display', 'block');
-    
     scoreBoard.html(score);
     showQuestion();
-    
     $('#timer').css('display', 'block');
     countDown(showingTimer);
-    
 };
 startTheGame.on("click",startGame);
 
@@ -170,24 +171,46 @@ function showQuestion(){
 
 //check the answer
 function checkAnswer(answer){
+    //right answer
     if (answer == questions[checkingQuestion].correct){
         score ++;
         scoreBoard.html(score);
+        //show the correct alert and the picture for right answer 
+        showAnswer.html('<h1>Correct!!!!!!</h1><br><img src="assets/images/'+ questions[checkingQuestion].pic + '">');
+        $('#timer').css('display', 'none');
+        quiz.css('display', 'none');
+        showAnswer.css('display','block');
+        timeout = setInterval(nextQuestion,4000);    
     }
+    //wrong
+    else if(answer !== questions[checkingQuestion].correct) {
+        showAnswer.html('<h1>Wrong!!!!!!</h1> <br><img src="assets/images/wrong.png"> ');
+        $('#timer').css('display', 'none');
+        quiz.css('display', 'none');
+        showAnswer.css('display','block');
+        
+        timeout = setInterval(nextQuestion,4000);
+    };
+};
+//next question
+function nextQuestion(){
     if (checkingQuestion < lastQuestion){
+        clearInterval(timeout);
         checkingQuestion ++;
         count = 20;
+        $('#timer').css('display', 'block');
+        quiz.css('display', 'block');
+        showAnswer.css('display','none');
         showQuestion();
         showingTimer();
     } else {
+        clearInterval(timeout);
         countDown= clearInterval(showingTimer);
         $('#timer').css('display', 'none');
         quiz.css('display', 'none');
-        result.css('display', 'block');
-        
-    }
-}
-
+        result.css('display', 'block');  
+    };
+};
 
 //Count down from 20;
 function countDown(showingTimer){
@@ -195,12 +218,17 @@ clearInterval(showingTimer);
 setInterval(showingTimer,1000);
 };
 //Timer
-
 function showingTimer(){
     if (count > 0){
         counter.html(count);
         count--;
     } else{
+        //if no choice is made and counter reaches 0
+        showAnswer.html("<h1>Time's up. Too slow!!!!!!</h1><br><img src='assets/images/wrong.png'>");
+        $('#timer').css('display', 'none');
+        quiz.css('display', 'none');
+        showAnswer.css('display','block');
+        timeout = setInterval(nextQuestion,4000);
         count = 20;
     };
 };
